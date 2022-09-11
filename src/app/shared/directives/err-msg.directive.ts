fd5d7ed72667c6b8cbfd53967996eca9203a1760
Agustín Casado <agu.e.casado@gmail.com>
2022-09-11 00:34:33 -0300

    cambiar texto de error desde la directiva personalizada

--- src/app/shared/directives/err-msg.directive.ts
+++ src/app/shared/directives/err-msg.directive.ts
@@ -7,18 +7,28 @@ export class ErrMsgDirective implements OnInit {
 
   constructor( private element: ElementRef<HTMLElement> ) {
     this.htmlElement = element;
-    console.log(this.errorMsgColor);
   }
   
   ngOnInit(): void {
     this.setColor();
+    this.setText();
+    this.setStyles();
   }
   
   @Input() errorMsgColor: string = 'red';
+  @Input() errorMsgText: string = '* required';
 
   htmlElement: ElementRef<HTMLElement>;
 
   setColor(): void {
     this.htmlElement.nativeElement.style.color = this.errorMsgColor;
+  }
+
+  setText(): void {
+    this.htmlElement.nativeElement.innerText = this.errorMsgText;
+  }
+
+  setStyles(): void {
+    this.htmlElement.nativeElement.classList.add('form-text')
   }
 }
