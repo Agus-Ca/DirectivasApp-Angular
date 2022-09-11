7fb7d0bd0b6835dcd4f71bee79eac1a428416523
Agustín Casado <agu.e.casado@gmail.com>
2022-09-11 00:27:48 -0300

    cambiar color del host desde la directiva

--- src/app/shared/directives/err-msg.directive.ts
+++ src/app/shared/directives/err-msg.directive.ts
@@ -1,16 +1,24 @@
-import { Directive, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
-  selector: '[error-msg]'
+  selector: '[errorMsg]'
 })
 export class ErrMsgDirective implements OnInit {
 
-  constructor( ) {
-    console.log('constructor Directive');
-   }
-
-  ngOnInit(): void {
-    console.log('ngOnInit Directive');
+  constructor( private element: ElementRef<HTMLElement> ) {
+    this.htmlElement = element;
+    console.log(this.errorMsgColor);
   }
+  
+  ngOnInit(): void {
+    this.setColor();
+  }
+  
+  @Input() errorMsgColor: string = 'red';
 
+  htmlElement: ElementRef<HTMLElement>;
+
+  setColor(): void {
+    this.htmlElement.nativeElement.style.color = this.errorMsgColor;
+  }
 }
